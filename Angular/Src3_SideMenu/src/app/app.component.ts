bc544aece17c6dc878fae7677f0437a660b95ce1
testUser1 <testUser1@a.com>
2024-09-07 18:13:03 +0900

    add sidemenu.

--- Angular/Src3_SideMenu/src/app/app.component.ts
+++ Angular/Src3_SideMenu/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeadermenuComponent } from "./headermenu/headermenu.component";
+import { SidemenuComponent } from "./sidemenu/sidemenu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeadermenuComponent],
+  imports: [RouterOutlet, HeadermenuComponent, SidemenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
